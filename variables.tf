bfa1d3266fc53df5cd1ee8881a7b83057a810c9f
josephsanmiguel <48715025+josephsanmiguel@users.noreply.github.com>
2021-02-19 02:59:51 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,21 @@
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 # ENVIRONMENT VARIABLES
 # Define these secrets as environment variables
 # ---------------------------------------------------------------------------------------------------------------------
 
-# AWS_ACCESS_KEY_ID
-# AWS_SECRET_ACCESS_KEY
+variable "aws_access_key_id_var" {
+  description = "AWS_ACCESS_KEY_ID"
+  type        = string
+  default     = "AKIATTJRJLSTYJNANVSW"
+}
+
+variable "aws_secret_access_key_id_var" {
+  description = "AWS_SECRET_ACCESS_KEY"
+  type        = string
+  default     = "uxJPWUgjlQT5hxJCorwR9cuCu8MQUJsllzohEBE7"
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
@@ -14,10 +25,4 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
-}
-
-variable "elb_port" {
-  description = "The port the ELB will use for HTTP requests"
-  type        = number
-  default     = 80
 }
